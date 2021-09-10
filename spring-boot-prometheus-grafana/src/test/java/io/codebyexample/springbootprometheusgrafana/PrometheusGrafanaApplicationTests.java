b1d5859b9a36d06c32471daa4cce7a5f8059c04b
huypva <huypva@vng.com.vn>
2021-09-10 15:09:54 +0700

    Rename package

--- spring-boot-prometheus-grafana/src/test/java/io/codebyexample/springbootprometheusgrafana/PrometheusGrafanaApplicationTests.java
+++ spring-boot-prometheus-grafana/src/test/java/io/codebyexample/springbootprometheusgrafana/PrometheusGrafanaApplicationTests.java
@@ -1,4 +1,4 @@
-package io.codebyexample.helloworld;
+package io.codebyexample.springbootprometheusgrafana;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
