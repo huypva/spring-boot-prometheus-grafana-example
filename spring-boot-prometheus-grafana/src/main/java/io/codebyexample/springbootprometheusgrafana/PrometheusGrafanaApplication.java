b04221aecdf5e20aaea8c1795982360cec693959
huypva <huypva@vng.com.vn>
2021-09-20 14:43:09 +0700

    Remove aop dependency

--- spring-boot-prometheus-grafana/src/main/java/io/codebyexample/springbootprometheusgrafana/PrometheusGrafanaApplication.java
+++ spring-boot-prometheus-grafana/src/main/java/io/codebyexample/springbootprometheusgrafana/PrometheusGrafanaApplication.java
@@ -1,21 +1,10 @@
 package io.codebyexample.springbootprometheusgrafana;
 
-import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
-@EnableAspectJAutoProxy
 public class PrometheusGrafanaApplication {
-
-	@Bean
-	TimedAspect timedAspect(MeterRegistry registry) {
-		return new TimedAspect(registry);
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrometheusGrafanaApplication.class, args);
